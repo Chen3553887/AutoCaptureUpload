00f112819503336fb410a591413a779767aed046
Nest <74977868+Chen3553887@users.noreply.github.com>
2023-09-14 21:07:20 +0800

    Update AutoCaptureUpload.py
    新增截图输出路径

--- AutoCaptureUpload.py
+++ AutoCaptureUpload.py
@@ -5,18 +5,19 @@ import requests
 import yaml
 
 
-def capture_screenshots(video_path, num_screenshots=3):
+def capture_screenshots(video_path, num_screenshots=3, output_dir=None):
     # 生成随机的截图时间点
     video_duration = get_video_duration(video_path)
     screenshot_times = random.sample(range(1, video_duration), num_screenshots)
 
     # 创建一个文件夹存储截图
-    screenshots_dir = os.path.dirname(video_path) + "/screenshots"
+    video_name = os.path.splitext(os.path.basename(video_path))[0]
+    screenshots_dir = os.path.join(output_dir, video_name)
     os.makedirs(screenshots_dir, exist_ok=True)
 
     # 使用ffmpeg截取视频截图
     for i, timepoint in enumerate(screenshot_times):
-        output_file = f'{screenshots_dir}/screenshot_{i+1}.jpg'
+        output_file = f'{screenshots_dir}/{video_name}_{i+1}.jpg'
         ffmpeg_command = ['ffmpeg', '-ss', str(timepoint), '-i', video_path, '-vframes', '1', '-q:v', '2', output_file]
         subprocess.run(ffmpeg_command)
 
@@ -56,13 +57,17 @@ def upload_to_image_hosting(image_path):
 # 获取拖入的视频文件路径
 video_path = input("请拖入视频文件：").strip('"')
 
-# 截取截图并保存到视频文件相同的路径
+# 读取配置文件中的输出路径
 with open("config.yml", "r") as file:
     config = yaml.safe_load(file)
-capture_screenshots(video_path,config.get("num_screenshots"))
+output_dir = config.get("output_dir")
+
+# 截取截图并保存到指定路径
+capture_screenshots(video_path, config.get("num_screenshots"), output_dir)
 
 # 遍历文件夹找到图片文件并上传到图床
-screenshots_dir = os.path.dirname(video_path) + "/screenshots"
+video_name = os.path.splitext(os.path.basename(video_path))[0]
+screenshots_dir = os.path.join(output_dir, video_name)
 bbcode_collection = []
 for filename in os.listdir(screenshots_dir):
     if filename.endswith(".jpg"):
@@ -74,5 +79,5 @@ for filename in os.listdir(screenshots_dir):
 
 print("所有图片返回的bbcode数据合集：\n")
 for bbcode in bbcode_collection:
-    print(bbcode)
+    print(bbcode+"\n")
 input("按任意键关闭窗口...")
